9bba01c6a28d8bb51ec6ab70dbbc2849d4c8f29a
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-06-05 06:43:40 +0200

    user controler do logowania

---
+++ Controllers/LoginAndRegisterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Profil_Osobowosci.Models;
+using System.Threading.Tasks;
+
+namespace Profil_Osobowosci.Controllers
+{
+    public class LoginAndRegisterController : Controller
+    {
+        // Dodaj tutaj zależność do serwisu obsługującego logowanie i rejestrację
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            // Implementacja logiki rejestracji
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+            // Implementacja logiki logowania
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace Profil_Osobowosci.Models;
+
+public class User:IdentityUser
+{
+    public string Name { get; set; }
+    public User(string name)
+    {
+        Name = name;
+    }
+
+    public User()
+    {
+    }
+}
